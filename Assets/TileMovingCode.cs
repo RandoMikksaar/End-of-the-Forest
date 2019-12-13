18526e0ba9fa91156aa495e04b8a0e8d90a483f2
Rando Mikksaar <23726279+RandoMikksaar@users.noreply.github.com>
2019-12-13 11:47:50 +0200

    som more stuff

---
+++ Assets/TileMovingCode.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileMovingCode : MonoBehaviour
+{
+    public GameObject selectedUnit;
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        ClickableTile ct = gameObject.GetComponent<ClickableTile>();
+
+    }
+
+    public void MoveSelectedUnitTo(int x, int y) {
+
+    }
+}
